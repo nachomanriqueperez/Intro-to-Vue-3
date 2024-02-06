5fae9f6f8b743f22c7b12d4263dbd5d3ae5b707b
nachomanriqueperez <nachomanriqueperez@gmail.com>
2024-02-06 16:47:24 +0100

    Primera leccion
    Añadida una descripcion

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return{
+            product: 'Socks',
+            description: 'Fantastic wool socks of different colours'
+        }
+    }
+})
